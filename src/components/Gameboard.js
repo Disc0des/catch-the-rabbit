8b16988f16306d26b146ec5cc5e9f0c36f18b560
DanHembery <93157414+DanHembery@users.noreply.github.com>
2022-05-24 10:42:46 +0100

    gameboard setup with styling

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -2,5 +2,22 @@ import React from "react";
 import "../styles/Gameboard.css";
 
 export default function Gameboard() {
-  return <div className="gameboard-container">Gameboard</div>;
+  const holesAmount = 50;
+  const holes = [];
+  for (let i = 1; i <= holesAmount; i += 1) {
+    holes.push(
+      <div className="hole-block">
+        <div id={i} className="game-hole" />
+        <h3>{i}</h3>
+      </div>
+    );
+  }
+
+  return (
+    <div className="gameboard-container">
+      <div className="overflow">
+        <div className="holes-grid">{holes}</div>
+      </div>
+    </div>
+  );
 }
